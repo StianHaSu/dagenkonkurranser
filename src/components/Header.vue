363a94aaad84784ffa9f2d879c44f014119d3247
stianhsu <stianhsu@uio.no>
2022-09-03 19:10:41 +0200

    nesten ferdig med banner

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,16 +4,23 @@
             visMobilMeny() {
                 var dropD = Array.from(document.getElementsByClassName('MobilValg') as HTMLCollectionOf<HTMLElement>)[0];
                 var hoved = Array.from(document.getElementsByClassName('bannerBg') as HTMLCollectionOf<HTMLElement>)[0];
+                var meny = Array.from(document.getElementsByClassName('Hamburger') as HTMLCollectionOf<HTMLElement>)[0];
+                var logo = Array.from(document.getElementsByClassName('DagenL') as HTMLCollectionOf<HTMLElement>)[0];
+
                 if (window.getComputedStyle(dropD).display === "none") {
                     dropD.setAttribute("style", "visibility: visible");
                     dropD.setAttribute("style", "display: inline");
                     hoved.setAttribute("style", "border-bottom-width: 1px");
-                    hoved.setAttribute("style", "height: 60%")
+                    hoved.setAttribute("style", "height: 90%");
+                    meny.setAttribute("style", "grid-row-start: 2");
+                    
                     
                 } else {
                     dropD.setAttribute("style", "visibility: invisible");
                     dropD.setAttribute("style", "display: none");
                     hoved.setAttribute("style", "border-bottom-width: 0px");
+                    meny.setAttribute("style", "grid-row-start: 3");
+                   
                 }
             },
         },
@@ -26,18 +33,21 @@
         <div class="outer2">
             <div class="bannerBg">
                 <div class="MobilMeny">
-                    <div class="col-start-5 row-start-2">
-                        <div><button @click="visMobilMeny()"><h1 class="rotate-[90deg]">|||</h1></button></div>
+                    <div class="DagenL">
+                        <img src="../../public/dagen_all_trans_white.png" alt="Dagenatifi logo">
                     </div>
-                </div>
-                <div class="MobilValg">
-                  <ul class="text-white text-bold font-mono text-lg py-2">
-                    <div class="border-b-2 w-4/5 mx-auto py-2 pt-8"><li><button @click="$router.push({ name: 'Prosjektskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out text-2xl">Konkurranse 1</button></li></div>
-                    <div class="border-b-2 w-4/5 mx-auto py-2"><li><button @click="$router.push({ name: 'Kontaktskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out text-2xl">Konkurranse 2</button></li></div>
-                    <div class="border-b-2 w-4/5 mx-auto py-2"><li><button @click="$router.push({ name: 'Omskjerm' })" class="hover:underline hover:scale-110 transition ease-in-out text-2xl">Konkurranse 3</button></li></div>
-                    <div class="border-b-2 w-4/5 mx-auto py-2"><li><button @click="$router.push({ name: 'CvSkjerm' })" class="hover:underline hover:scale-110 transition ease-in-out text-2xl">Konkurranse 4</button></li></div>
-                    <div class="pb-4"></div>
-                  </ul>
+                    <div class="Hamburger">
+                        <button @click="visMobilMeny()"><h1 class="rotate-[90deg] hover:scale-110">|||</h1></button>
+                    </div>
+                    <div class="MobilValg">
+                        <ul class="text-white text-bold font-mono text-lg py-12">
+                          <div class="border-b-2 w-4/5 mx-auto py-2 pt-8"><li><button @click="$router.push({ name: 'Prosjektskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out text-2xl">Konkurranse 1</button></li></div>
+                          <div class="border-b-2 w-4/5 mx-auto py-2"><li><button @click="$router.push({ name: 'Kontaktskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out text-2xl">Konkurranse 2</button></li></div>
+                          <div class="border-b-2 w-4/5 mx-auto py-2"><li><button @click="$router.push({ name: 'Omskjerm' })" class="hover:underline hover:scale-110 transition ease-in-out text-2xl">Konkurranse 3</button></li></div>
+                          <div class="border-b-2 w-4/5 mx-auto py-2"><li><button @click="$router.push({ name: 'CvSkjerm' })" class="hover:underline hover:scale-110 transition ease-in-out text-2xl">Konkurranse 4</button></li></div>
+                          <div class="pb-4"></div>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -63,29 +73,91 @@
     bg-color2
     rounded-b-2xl
     sticky top-0
+    shadow-lg
+    shadow-color2
     
 }
 
 .MobilValg {
     @apply
     hidden
-    shadow-md
     rounded-3xl
     w-screen
     text-center
+    row-start-3
     
 }
 
 .MobilMeny {
     @apply
-    text-3xl
+    text-4xl
     font-sans
     font-bold
     text-white
-    hover:scale-110
-    self-center
-    grid columns-7
-    grid-rows-3
+    grid
+    grid-cols-10
+    grid-rows-6
+    h-full
     
+    
+}
+
+.Hamburger{
+    @apply
+    col-start-9 
+    row-start-3 
+    col-span-1 
+    row-span-1
+}
+
+.DagenL{
+    @apply
+    col-start-2
+    row-start-2
+    col-span-3
+    row-span-1
+}
+
+@media only screen and (min-width: 600px) {
+    .bannerBg{
+        @apply
+        h-[33%]
+    }
+
+    .MobilMeny{
+        @apply
+        text-5xl
+    }
+
+   
+
+}
+
+@media only screen and (min-width: 900px){
+    .DagenL{
+        @apply
+        scale-[80%]
+    }
+}
+
+@media only screen and (min-width: 1000px){
+    .DagenL{
+        @apply
+        scale-[70%]
+        
+     
+    }
+
+    .Hamburger{
+        @apply
+        text-[4rem]
+    }
+}
+
+@media only screen and (min-width: 1300px){
+    .DagenL{
+        @apply
+        scale-[60%]
+    }
 }
 </style>
